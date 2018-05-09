535313d6df09e8425900b8521bbfd1aca02ce8f7
William Arellano <william.arellano@yachaytech.edu.ec>
2018-05-09 11:47:28 -0500

    First Commit

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class BoardController : MonoBehaviour {
+
+    public Button tilePrefab;
+    private int cap = 25;
+    private int score;
+    private int attempts;
+    private Text scoreText;
+    private Text attemptsText;
+    private int[] numbers = new int[25 * 2];
+
+    private Button fstSelectedTile;
+    private Button sndSelectedTile;
+
+    void Awake()
+    {
+        InitState();
+        // Set Cap for FPS.
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 15;
+    }
+
+	void Start ()
+    {
+        scoreText = GameObject.Find("Score").GetComponent<Text>();
+        attemptsText = GameObject.Find("Attempts").GetComponent<Text>();
+        Button resetButton = GameObject.Find("ResetButton").GetComponent<Button>();
+        resetButton.onClick.AddListener(delegate { Reset(); });
+        StartCoroutine(InitTiles());
+	}
+	
+	void Update () {
+        scoreText.text = "Score: " + score.ToString();
+        attemptsText.text ="Attempts: " + attempts.ToString();
+
+        if (fstSelectedTile != null)
+        {
+            if (sndSelectedTile != null)
+            {
+                if (CheckSelection())
+                {
+                    fstSelectedTile.GetComponent<TileController>().Hide();
+                    sndSelectedTile.GetComponent<TileController>().Hide();
+                    score++;
+                }
+                else
+                {
+                    IEnumerator fstTileDeselectCoroutine = fstSelectedTile.GetComponent<TileController>().Deselect();
+                    IEnumerator sndTileDeselectCoroutine = sndSelectedTile.GetComponent<TileController>().Deselect();
+                    StartCoroutine(fstTileDeselectCoroutine);
+                    StartCoroutine(sndTileDeselectCoroutine);
+                    attempts++;
+                }
+                fstSelectedTile = null;
+                sndSelectedTile = null;
+            }
+
+        }
+		
+	}
+
+    void TileClick (string tileName)
+    {
+        Button clickedTile = GameObject.Find(tileName).GetComponent<Button>();
+        clickedTile.GetComponent<TileController>().Select();
+        if (fstSelectedTile == null && sndSelectedTile == null)
+        {
+            fstSelectedTile = clickedTile;
+        }
+        else
+        {
+            if (sndSelectedTile == null)
+            {
+                sndSelectedTile = clickedTile;
+            }
+        }
+    }
+
+    bool CheckSelection()
+    {
+        int fstTileNumber = fstSelectedTile.GetComponent<TileController>().number;
+        int sndTileNumber = sndSelectedTile.GetComponent<TileController>().number;
+        
+        if(fstTileNumber == sndTileNumber)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void AddTile(Button tile)
+    {
+        if (tile == null)
+        {
+            return;
+        }
+
+        tile.transform.SetParent(gameObject.transform, false);
+    }
+
+    void Reset ()
+    {
+        DeleteTiles();
+        InitState();
+        StartCoroutine(InitTiles());
+    }
+
+    void InitState()
+    {
+        // Generates the list of numbers the numbers appear twice.
+        for (int i = 0; i < cap; i++)
+        {
+            numbers[i] = i;
+            numbers[cap + i] = i;
+        }
+        // Shuffles the number's list.
+        System.Random rng = new System.Random();
+        int n = numbers.Length;
+        while (n > 1)
+        {
+            int h = rng.Next(0, n);
+            n--;
+            int temp = numbers[h];
+            numbers[h] = numbers[n];
+            numbers[n] = temp;
+        }
+        attempts = 0;
+        score = 0;
+    }
+
+    // Deletes the Tiles from game.
+    void DeleteTiles ()
+    {
+        for(int i=0; i < 2*cap; i++)
+        {
+            GameObject obj = GameObject.Find(i.ToString());
+            Destroy(obj);
+        }
+    }
+
+    // Create and draw tiles.
+    IEnumerator InitTiles ()
+    {
+
+        // Creates tiles and adds it to the gridlayout.
+        int tileName = 0;
+        foreach (var number in numbers)
+        {
+            Button tile = Instantiate(tilePrefab);
+            tile.GetComponentInChildren<TileController>().number = number;
+            tile.name = tileName.ToString();
+            tile.GetComponent<Button>().onClick.AddListener(delegate { TileClick(tile.name); });
+            AddTile(tile);
+            tileName++;
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileController : MonoBehaviour {
+
+    public Sprite tileNormal;
+    public Sprite tileSelected;
+    public Sprite tileHidden;
+
+    public int number;
+    private bool selected;
+    private bool hidden;
+
+    private Color normalTileColor = new Color(72.5f, 72.5f, 72.2f);
+    private Color selectedTileColor = new Color(91.4f, 38.4f, 11.8f);
+
+	void Start () {
+        gameObject.GetComponentInChildren<Text>().text = "";
+        gameObject.GetComponent<Image>().color = selectedTileColor;
+        
+    }
+
+    void Awake() {
+       selected = false;
+       hidden = false;
+    }
+
+    void Update()
+    {
+        if (hidden)
+        {
+            gameObject.GetComponentInChildren<Image>().sprite = tileHidden;
+            gameObject.GetComponentInChildren<Image>().canvasRenderer.SetAlpha(0);
+            gameObject.GetComponentInChildren<Text>().text = "";
+            gameObject.GetComponent<Button>().enabled = false;
+        }
+        else
+        {
+            if (!selected)
+            {
+                gameObject.GetComponentInChildren<Image>().sprite = tileNormal;
+                gameObject.GetComponentInChildren<Text>().text = "";
+            }
+            else
+            {
+                gameObject.GetComponentInChildren<Image>().sprite = tileSelected;
+                gameObject.GetComponentInChildren<Text>().text = number.ToString();
+            }
+        }
+        
+    }
+
+    public void Hide()
+    {
+        hidden = true;
+    }
+
+    public void Select()
+    {
+        selected = true;
+    }
+
+    public IEnumerator Deselect()
+    {
+        yield return new WaitForSeconds(1f);
+        selected = false;
+    }
+    
+    IEnumerator ChangeColorAnimation()
+    {
+        float animationTime = 0.3f;
+        int steps = 20;
+
+        float deltaR = (selectedTileColor.r - normalTileColor.r)/steps;
+        float deltaG = (selectedTileColor.g - normalTileColor.g)/steps;
+        float deltaB = (selectedTileColor.b - normalTileColor.b)/steps;
+
+        float deltaTime = animationTime / steps;
+
+        for (int i=0; i<steps; i++)
+        {
+            float red = selectedTileColor.r + deltaR * i;
+            float green = selectedTileColor.g + deltaG * i;
+            float blue = selectedTileColor.b + deltaB * i;
+
+            Color tileColor = new Color(red, green, blue, 1f);
+            gameObject.GetComponentInChildren<Image>().color = tileColor;
+            yield return new WaitForSeconds(deltaTime);
+        }
+        gameObject.GetComponent<Image>().color = Color.white;
+        gameObject.GetComponentInChildren<Image>().sprite = tileNormal;
+        yield return null;
+
+    }
+
+}
